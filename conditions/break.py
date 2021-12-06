5c2557ad274d674dd4789922102791da9b1cc263
charan <sumacharan.adabala@gmail.com>
2021-12-06 20:59:36 +0530

    added new pythonfolder

---
+++ conditions/break.py
@@ -0,0 +1,12 @@
+count = eval(input("Enter the count"))
+
+while count < 10:
+	count+=1
+	if count == 5:
+		break
+			
+print(count)   	
+
+	
+
+	
---
+++ variables/typecasting.py
@@ -0,0 +1,59 @@
+# number = "10"
+# num = int(number)
+
+value = "10"
+number = int(value)
+
+# pi_string = "3.1415926"
+
+# # pi_float = (str(pi_string))
+# # print(pi_float)
+# x_click = float(str(pi_string))
+
+# print(x_click)
+
+# area variables (in square meters)
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.50
+
+# Adapt list areas
+# areas=["hallway", hall, "kitchen", kit]
+# print(areas)
+
+# list = [1,3,4,2]
+# print(list)
+
+# list1 = [[1,2,3],[4,5,7]]
+# print(list1)
+# list2 = [1+2, "a" * 5, 3]
+# print(list2)
+
+# Create the areas list
+# areas = ["hallway", 11.25, "kitchen", 18.0, "living room",
+#          20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# # Sum of kitchen and bedroom area: eat_sleep_area
+# eat_sleep_area = ["kitchen", 18.0, "bedroom", 10.75]
+# # Print the variable eat_sleep_area
+# print(eat_sleep_area[0])
+# print(eat_sleep_area[1])
+# print(eat_sleep_area[2])
+# print(eat_sleep_area[3])
+# print("------------------")
+# print(eat_sleep_area[-1])
+# print(eat_sleep_area[-2])
+# print(eat_sleep_area[-3])
+# print(eat_sleep_area[-4])
+# print("--------------------")
+# print(eat_sleep_area[1] + eat_sleep_area[3])
+# print("--------------------")
+# print(eat_sleep_area)
+# x = ["a", "b", "c", "d"]
+# print(x[1] + x[3])
+x = ["a", "b", "c", "d"]
+print(x[:2])
+print(x[2:])
+print(x[:])
